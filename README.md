d687d602b768be2e05bd7b9076ddfa6d30de0ef9
Jacco Flenter <github@putpixel.nl>
2015-11-03 10:37:44 +0100

    switch to api.nodesecurity.io

--- README.md
+++ README.md
@@ -8,7 +8,7 @@ This step will send the shrinkwrap file to nodesecurity.io. If any vulnerable mo
 
 # Example
 
-This example will run the step using the default settings: 
+This example will run the step using the default settings:
 
 ```
 build:
@@ -27,7 +27,26 @@ build:
 
 # What's new
 
-- Initial release.
+Switched api call to api.nodesecurity.io. Output has also been improved showing the path of the module
+that has the vulnerability. Example output:
+
+``` text
+2 vulnerabilities/issues detected:
+
+Title:    Denial-of-Service Extended Event Loop Blocking
+Module:   qs
+version:  0.6.6
+Patched:  >= 1.x
+Path:     request > qs
+Advisory: https://nodesecurity.io/advisories/28
+
+Title:    Denial-of-Service Memory Exhaustion
+Module:   qs
+version:  0.6.6
+Patched:  >= 1.x
+Path:     request > qs
+Advisory: https://nodesecurity.io/advisories/29
+```
 
 # Options
 
@@ -42,6 +61,10 @@ TODO
 The MIT License (MIT)
 
 # Changelog
+
+## 2.0.0
+
+- update api call to api.nodescurity.io
 
 ## 1.0.0
 
--- bin/security_audit.js
+++ bin/security_audit.js
@@ -1,42 +1,55 @@
+var colors = require('colors');
 var fs = require('fs');
 var request = require('request');
 var underscore = require('underscore');
 
 var shrinkwrapPath = process.argv[2];
-var shrinkwrapStream = fs.createReadStream(shrinkwrapPath);
+var shrinkwrapData = JSON.parse(fs.readFileSync(shrinkwrapPath));
+
+// Api call requires a package.json. A nice extra feature would be to allow
+// the step to use a real package.json
+var staticPackageJSON = {
+  name: 'wercker/step-shrinkwrap-security-audit'
+};
 
 var requestOptions = {
-    'uri': 'https://nodesecurity.io/validate/shrinkwrap',
-    'method': 'POST',
-    'json': true,
-    'headers': {
-        'Content-type': 'application/json'
-    }
+  uri: 'https://api.nodesecurity.io/check',
+  method: 'POST',
+  json: true,
+  body: {
+    shrinkwrap: shrinkwrapData,
+    package: staticPackageJSON
+  },
+  headers: {
+    'Content-type': 'application/json'
+  }
 };
 
 var post = request(requestOptions, function(err, resp, body) {
-    'use strict';
-    if (err) {
-        console.error('Unable to make request');
-        process.exit(33);
-    }
+  'use strict';
+  if (err) {
+    console.error('Unable to make request');
+    process.exit(33);
+  }
 
-    if (!body || !underscore.isArray(body)) {
-        console.error('Expecting response to be a json array');
-        process.exit(34);
-    }
+  if (!body || !underscore.isArray(body)) {
+    console.error('Expecting response to be a json array');
+    process.exit(34);
+  }
 
-    if (body.length === 0) {
-        console.log('No vulnerable modules detected.');
-        process.exit(0);
-    }
-    else {
-        console.log('Vulnerable modules detected:');
-        body.forEach(function(vulnerability) {
-            console.log('Module ' + vulnerability.module + '@' + vulnerability.version + ' has a vulnerability (more information: https://nodesecurity.io/advisories/' + vulnerability.advisory.url + ')')
-        });
-        process.exit(9);
-    }
+  if (body.length === 0) {
+    console.log('No vulnerable modules detected.'.green);
+    process.exit(0);
+  }
+
+  console.log('\n' + body.length.toString().red.bold + ' vulnerabilities/issues detected: \n'.bold);
+  body.forEach(function(vulnerability) {
+    console.log('Title:    ' + vulnerability.title);
+    console.log('Module:   ' + vulnerability.module);
+    console.log('version:  ' + vulnerability.version);
+    console.log('Patched:  ' + vulnerability.patched_versions);
+    console.log('Path:     ' + vulnerability.path.join(' > '));
+    console.log('Advisory: ' + vulnerability.advisory + '\n');
+  });
+  process.exit(9);
 });
-
-shrinkwrapStream.pipe(post);
